271046d0e393d4a27e36a6afdb1b62d4a1522539
Magine Icer Lee <con@loli.lu>
2015-09-21 10:08:11 +0800

    patch urls

--- home/views.py
+++ home/views.py
@@ -3,3 +3,23 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'home/index.html')
+
+
+def about(request):
+    return render(request, 'home/about.html')
+
+
+def experience(request):
+    return render(request, 'home/experience.html')
+
+
+def china(request):
+    return render(request, 'home/china.html')
+
+
+def accommodations(request):
+    return render(request, 'home/accommodations.html')
+
+
+def contact(request):
+    return render(request, 'home/contact.html')
