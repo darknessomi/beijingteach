c9b9edfd4a2810eaefb6d8d3ca75218bc004c0f1
limincheng <con@loli.lu>
2015-12-01 12:59:22 +0800

    add dashboard pages

--- core/urls.py
+++ core/urls.py
@@ -1,25 +1,16 @@
-"""core URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.8/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Add an import:  from blog import urls as blog_urls
-    2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
-"""
 from django.conf.urls import include, url
 from django.contrib import admin
 
 from home import views as home
+from dashboard import views as dashboard
 
 urlpatterns = [
+    # plugin apps
     url(r'^admin/', include(admin.site.urls)),
+]
+
+urlpatterns += [
+    # user pages (home app)
     url(r'^$', home.index, name='index'),
     url(r'^about/$', home.about, name='about'),
     url(r'^experience/$', home.experience, name='experience'),
@@ -27,4 +18,9 @@ urlpatterns = [
     url(r'^accommodations/$', home.accommodations, name='accommodations'),
     url(r'^contact/$', home.contact, name='contact'),
     url(r'^apply/$', home.contact, name='apply'),
+]
+
+urlpatterns += [
+    # dashboard pages (dashboard app)
+    url(r'^dashboard/$', dashboard.index),
 ]
